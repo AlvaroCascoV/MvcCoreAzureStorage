fb4e3f46cd5ffc482987e409649a5af5cf13f734
AlvaroCascoV <alvarocascovalero@gmail.com>
2026-04-13 13:01:19 +0200

    blob y container crud

---
+++ MvcCoreAzureStorage/Controllers/AzureBlobsController.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcCoreAzureStorage.Models;
+using MvcCoreAzureStorage.Services;
+using System.Threading.Tasks;
+
+namespace MvcCoreAzureStorage.Controllers
+{
+    public class AzureBlobsController : Controller
+    {
+        private ServiceStorageBlobs service;
+
+        public AzureBlobsController(ServiceStorageBlobs service)
+        {
+            this.service = service;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            List<string> containers = await this.service.GetContainersAsync();
+            return View(containers);
+        }
+
+        public IActionResult CreateContainer()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateContainer(string containerName)
+        {
+            await this.service.CreateContainerAsync(containerName);
+            return RedirectToAction("Index");
+        }
+        public async Task<IActionResult> DeleteContainer(string containerName)
+        {
+            await this.service.DeleteContainerAsync(containerName);
+            return RedirectToAction("Index");
+        }
+
+        public async Task<IActionResult> ListBlobs(string containerName)
+        {
+            ViewData["CONTAINER"] = containerName;
+            List<BlobModel> models = await this.service.GetBlobsAsync(containerName);
+            return View(models);
+        }
+
+        public IActionResult UploadBlob(string containerName)
+        {
+            ViewData["CONTAINER"] = containerName;
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UploadBlob(string containerName, IFormFile file)
+        {
+            string blobName = file.FileName;
+            using (Stream stream = file.OpenReadStream())
+            {
+                await this.service.UploadBlobAsync(containerName, blobName, stream);
+            }
+            return RedirectToAction("ListBlobs", new { containerName = containerName });
+        }
+
+        public async Task<IActionResult> DeleteBlob(string containerName, string blobName)
+        {
+            await this.service.DeleteBlobAsync(containerName, blobName);
+            return RedirectToAction("ListBlobs", new { containerName = containerName });
+        }
+    }
+}
---
+++ MvcCoreAzureStorage/Services/ServiceStorageBlobs.cs
@@ -0,0 +1,73 @@
+﻿using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+using MvcCoreAzureStorage.Models;
+
+namespace MvcCoreAzureStorage.Services
+{
+    public class ServiceStorageBlobs
+    {
+        private BlobServiceClient client;
+
+        public ServiceStorageBlobs(BlobServiceClient client)
+        {
+            this.client = client;
+        }
+
+        //METODO PARA RECUPERAR TODOS LOS CONTAINERS
+        public async Task<List<string>> GetContainersAsync()
+        {
+            List<string> containers = new List<string>();
+            await foreach (BlobContainerItem item in this.client.GetBlobContainersAsync())
+            {
+                containers.Add(item.Name);
+            }
+            return containers;
+        }
+
+        //METODO PARA CREAR UN CONTAINER
+        public async Task CreateContainerAsync(string containerName)
+        {
+            await this.client.CreateBlobContainerAsync(containerName.ToLower(), PublicAccessType.None); //se cambia el tipo del container para controlar el acceso
+        }
+
+        //ELIMINAR UN CONTAINER
+        public async Task DeleteContainerAsync(string containerName)
+        {
+            await this.client.DeleteBlobContainerAsync(containerName);
+        }
+
+        //LISTADO DE FICHEROS DENTRO DE UN CONTAINER
+        public async Task<List<BlobModel>> GetBlobsAsync(string containerName)
+        {
+            //NECESITAMOS UN CLIENTE DE BLOBS CONTAINER
+            //PARA EL ACCESO A LOS FICHEROS
+            BlobContainerClient containerClient = this.client.GetBlobContainerClient(containerName);
+            List<BlobModel> blobs = new List<BlobModel>();
+            await foreach (BlobItem item in containerClient.GetBlobsAsync())
+            {
+                //necesito este otro client para recuperar la url del blob
+                BlobClient blobClient = containerClient.GetBlobClient(item.Name);
+                BlobModel model = new BlobModel();
+                model.Nombre = item.Name;
+                model.Container = containerName;
+                model.Url = blobClient.Uri.AbsoluteUri;
+                blobs.Add(model);
+            }
+            return blobs;
+        }
+
+        //METODO PARA ELIMINAR UN BLOB
+        public async Task DeleteBlobAsync(string containerName, string blobName)
+        {
+            BlobContainerClient blobContainerClient = this.client.GetBlobContainerClient(containerName);
+            await blobContainerClient.DeleteBlobAsync(blobName);
+        }
+
+        //SUBIR UN BLOB A UN CONTAINER
+        public async Task UploadBlobAsync(string containerName, string blobName, Stream stream)
+        {
+            BlobContainerClient blobContainerClient = this.client.GetBlobContainerClient(containerName);
+            await blobContainerClient.UploadBlobAsync(blobName, stream);
+        }
+    }
+}
